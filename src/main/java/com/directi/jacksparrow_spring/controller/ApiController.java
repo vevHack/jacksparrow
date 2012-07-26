9d723e3a31e4fbaf0574f143a2303d46c3f8acf7
Manav Rathi <manav.r@directi.com>
2012-07-26 12:41:21 +0530

    Add postgres version to ping

--- src/main/java/com/directi/jacksparrow_spring/controller/ApiController.java
+++ src/main/java/com/directi/jacksparrow_spring/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.directi.jacksparrow_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.SpringVersion;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +15,8 @@ import java.util.Map;
 @RequestMapping("/api")
 public class ApiController {
 
+    private @Autowired JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/ping")
     @ResponseBody
     public Map<String, Object> sayHi() {
@@ -21,8 +25,14 @@ public class ApiController {
             put("timestamp", new Timestamp(System.currentTimeMillis()));
             put("version", new HashMap<String, Object>() {{
                 put("spring", getSpringVersion());
+                put("postgres", getPostgresVersion());
             }});
         }};
+    }
+
+    private String getPostgresVersion() {
+        return (String)jdbcTemplate.queryForMap("SELECT VERSION()")
+                .get("version");
     }
 
     private String getSpringVersion() {
