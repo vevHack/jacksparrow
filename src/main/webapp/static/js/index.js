c6a69623bd89e60e0fb9236fb2fc25fbf3ca9e3f
Manav Rathi <manav.r@directi.com>
2012-08-06 13:38:46 +0530

    Logout JS complete

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -1,51 +1,20 @@
+var jks = jks || {};
+
 (function() {
+    "use strict";
 
     function preload() {
         $.fetch.template("register");
         $.fetch.template("users");
     }
 
-    var onRegisterClick = (function() {
-        var action;
-
-        function firstTime() {
-            $.fetch.template("register").done(function(template) {
-                $("#register").append($(Mustache.render(template)).hide())
-                show();
-                action = hide;
-            });
-            action = pending;
-        }
-
-        function hide() {
-            $("#register >form").hide("slow", function() {
-                action = show;
-            });
-        }
-
-        function show() {
-            $("#register >form").show("slow", function() {
-                action = hide;
-            });
-        }
-
-        function pending() {
-            $("#info").hide()
-                .html(
-                    $("<span>The goodies are on their way..., Please wait.<span>"))
-                .show("slow", function() {
-                    setTimeout($.proxy(function() {
-                        this.hide("slow");
-                    }, $(this)), 2000)
-                });
-        }
-
-        action = firstTime;
-        return function () {
-            action();
-            return false;
-        }
-    }());
+    function logout() {
+        $.post("/api/session/destroy")
+            .fail(jks.common.warn)
+            .done(function() {
+                window.location.replace("/");
+            })
+    }
 
     var onUsersClick = (function() {
         var action;
@@ -85,7 +54,7 @@
         $.fetch.template("index").done(function(template) {
                 preload();
                 $("body").html(Mustache.render(template));
-                $("#register").on("click", ".trigger", onRegisterClick);
+                $("#logout").on("click", ".trigger", logout);
                 $("#users").on("click", ".trigger", onUsersClick);
             });
     });
