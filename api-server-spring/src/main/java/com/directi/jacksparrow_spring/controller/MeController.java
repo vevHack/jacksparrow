aade155beb93e38a2f2cfad6a923576b7e35351b
Manav Rathi <manav.r@directi.com>
2012-08-06 16:29:41 +0530

    Self information (me) API

---
+++ api-server-spring/src/main/java/com/directi/jacksparrow_spring/controller/MeController.java
@@ -0,0 +1,40 @@
+package com.directi.jacksparrow_spring.controller;
+
+import com.directi.jacksparrow_spring.exception.EntityNotFoundException;
+import com.directi.jacksparrow_spring.exception.PreconditionViolatedException;
+import com.directi.jacksparrow_spring.exception.UserAuthorizationException;
+import com.directi.jacksparrow_spring.repository.UserRepository;
+import com.directi.jacksparrow_spring.service.Authorizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/api/me")
+@SuppressWarnings("unchecked")
+public class MeController {
+
+    private @Autowired Authorizer authorizer;
+    private @Autowired UserRepository userRepository;
+
+    @RequestMapping()
+    @ResponseBody
+    public Map me() throws EntityNotFoundException, UserAuthorizationException {
+        /* XXX: inefficient? will run everytime */
+        try {
+            return new HashMap() {{
+                put("user", userRepository.details(
+                        authorizer.getAuthorizedUser().getId(),
+                        Arrays.asList(new String[]{"username"})));
+            }};
+        } catch (PreconditionViolatedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
---
+++ test/me.js
@@ -0,0 +1,27 @@
+var $ = require("jquery");
+var should = require("should");
+var config = require("./util/config");
+var common = require("./util/common");
+var ajaxWithCookieFactory = require("./util/ajaxWithCookieFactory")
+
+
+describe("Authorization", function() {
+    
+    before(common.isServerUp.bind(common));
+
+    it("should get self using Authentication Header", function(done) {
+        common.createSession()
+            .done(function(access_token) {
+                common.authJson(access_token, config.url("/api/me"))
+                    .done(function(data) {
+                        data.should.have.property("user");
+                        data.user.should.have.property("id");
+                        data.user.should.have.property("username");
+                        data.user.id.should.equal(config.testUser.id);
+                    })
+                    .fail(common.shouldNotFail)
+                    .always(function(){done()});
+            });
+    });
+
+});
